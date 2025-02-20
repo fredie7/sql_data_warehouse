d722177bef6b21eacf74e1dfc1322de45b3765fc
fredie7 <52379604+fredie7@users.noreply.github.com>
2025-02-20 13:10:21 +0200

    Update silver_customers.sql

--- silver_layer/silver_customers.sql
+++ silver_layer/silver_customers.sql
@@ -1,6 +1,6 @@
 -- QUALITY CHECKS
 -- Check for unnecessary spaces
-select email from bronze_customers where email != trim(email);
+SELECT email FROM bronze_customers WHERE email != TRIM(email);
 
 -- Check for unwanted brackets within the nationality
 SELECT nationality
@@ -25,26 +25,29 @@ nationality VARCHAR(50)
 -- Truncate  before insert
 TRUNCATE table silver_customers;
 INSERT INTO silver_customers
-SELECT customer_id,customer_name,
-LOWER(TRIM(email)) as email,
-CASE
-WHEN LENGTH(REGEXP_REPLACE(phone_number,'[^0-9]','')) >= 10 
-THEN CONCAT(
-SUBSTRING(REGEXP_REPLACE(phone_number,'[^0-9]',''),1,3),'-',
-SUBSTRING(REGEXP_REPLACE(phone_number,'[^0-9]',''),4,3),'-',
-SUBSTRING(REGEXP_REPLACE(phone_number,'[^0-9]',''),7,3)
-)
-ELSE REGEXP_REPLACE(phone_number,'[^0-9]','')
-END AS phone_number,
-loyalty_status,
-REGEXP_SUBSTR(nationality,'^[^()]+') as nationality from bronze_customers;
+SELECT 
+  customer_id,
+  customer_name,
+  LOWER(TRIM(email)) as email,
+  CASE
+    WHEN LENGTH(REGEXP_REPLACE(phone_number,'[^0-9]','')) >= 10 
+    THEN CONCAT(
+    SUBSTRING(REGEXP_REPLACE(phone_number,'[^0-9]',''),1,3),'-',
+    SUBSTRING(REGEXP_REPLACE(phone_number,'[^0-9]',''),4,3),'-',
+    SUBSTRING(REGEXP_REPLACE(phone_number,'[^0-9]',''),7,3)
+    )
+    ELSE REGEXP_REPLACE(phone_number,'[^0-9]','')
+  END AS phone_number,
+  loyalty_status,
+  REGEXP_SUBSTR(nationality,'^[^()]+') as nationality 
+FROM bronze_customers;
 
 -- View the first 5 rows
-select * from silver_customers limit 5;
+SELECT * FROM silver_customers LIMIT 5;
 
 -- QUALITY CHECKS
 -- Check for unnecessary spaces
-select email from silver_customers where email != TRIM(email);
+SELECT email FROM silver_customers WHERE email != TRIM(email);
 
 -- Check for unwanted brackets within the nationality
 SELECT nationality
@@ -52,13 +55,13 @@ FROM silver_customers
 WHERE REGEXP_LIKE(nationality, '[\(\)\{\}]');
 
 -- View the first 5 rows
-select * from silver_customers limit 5;
+SELECT * FROM silver_customers LIMIT 5;
 
 
 
 -- QUALITY CHECKS
 -- Check for unnecessary spaces
-select email from silver_customers where email != TRIM(email);
+SELECT email FROM silver_customers WHERE email != TRIM(email);
 
 -- Check for unwanted brackets within the nationality
 SELECT nationality
