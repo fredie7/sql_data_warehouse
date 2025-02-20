ab25dd45823eb2241e28bfc9ab1c18777a420e82
fredie7 <52379604+fredie7@users.noreply.github.com>
2025-02-20 22:10:46 +0200

    Update silver_reservations.sql

--- silver_layer/silver_reservations.sql
+++ silver_layer/silver_reservations.sql
@@ -1,8 +1,12 @@
 -- Check for invalid order dates
 SELECT * FROM bronze_reservations where check_out < check_in;
 
+-- Check for any date greater than the current date
+SELECT * FROM bronze_reservations where check_out > current_date() AND check_in > current_date();
+
 -- Create & insert transformed data into silver_reservations
-CREATE TABLE silver_reservations(
+
+CREATE TABLE IF NOT EXISTS silver_reservations(
 reservation_id INT PRIMARY KEY,
 customer_id INT,
 room_id INT,
